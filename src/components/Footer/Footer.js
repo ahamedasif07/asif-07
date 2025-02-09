16f23a507db0cc00b9a4907c20ecdbab4b058d26
rxasif07 <rxasif31@gmail.com>
2025-02-09 08:24:02 +0600

    data done

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+import logo from "../../../public/assets/alogo.svg";
+
+const Footer = () => {
+  return (
+    <div className="border-t border-gray-300 ">
+      <div className="max-w-screen-2xl px-4 py-[30px] mx-auto flex flex-col justify-center place-items-center">
+        <Image height={100} width={100} src={logo} alt="logo"></Image>
+        <p className="mt-2 text-[13px] text-gray-400">
+          ©2025 Copyright All Rights Reserved
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
