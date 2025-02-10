5076cc704e1c8ca598be9fd0231e2d34c4e2e375
rxasif07 <rxasif31@gmail.com>
2025-02-10 08:34:04 +0600

    contact from remake and responcive

--- src/components/ContactFrom/ContactFrom.js
+++ src/components/ContactFrom/ContactFrom.js
@@ -2,6 +2,9 @@
 
 import { useState } from "react";
 import SectionTitle from "../SectionTitle/SectionTitle";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaGithub, FaGrimace } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -27,62 +30,85 @@ const ContactForm = () => {
       </div>
       <div className=" flex flex-col max-w-screen-2xl items-center justify-center py-5 ">
         <form
-          className="bg-gray-800 border-2 border-blue-600 bg-transparent  p-10 rounded-lg shadow-lg w-full max-w-[800px] "
+          className="bg-gray-800 border-2 flex md:flex-row flex-col gap-5 md:gap-[40px] items-center border-blue-600 bg-transparent p-5 md:p-10 rounded-lg shadow-lg w-full max-w-[800px] "
           onSubmit={handleSubmit}
         >
-          <div className="mb-4">
-            <label className="block text-gray-200">
-              <span className="text-gray-200">Name</span>
-            </label>
-            <input
-              type="text"
-              name="name"
-              required
-              value={formData.name}
-              onChange={handleChange}
-              className="w-full p-3 bg-gray-800 text-gray-200 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+          <div className="flex flex-col gap-5">
+            <h2 className="px-4 py-[10px] border-[2px] border-blue-600">
+              <h3 className="text-gray-400 text-[26px] pb-[6px] flex justify-center">
+                <MdOutlineEmail />
+              </h3>
+              ahamedasif01729@gmail.com
+            </h2>
+
+            <h2 className="px-4 py-[10px] border-[2px] border-blue-600">
+              <h3 className="text-gray-400 text-[26px] pb-[6px] flex justify-center">
+                <FaGithub />
+              </h3>
+              ahamedasif01729@gmail.com
+            </h2>
+            <h2 className="px-4 py-[10px] border-[2px] border-blue-400">
+              <h3 className="text-gray-400 text-[26px] pb-[6px] flex justify-center">
+                <FaWhatsapp />
+              </h3>
+              ahamedasif01729@gmail.com
+            </h2>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div>
-              <label className="block text-gray-200">Email</label>
-              <input
-                type="email"
-                name="email"
-                required
-                value={formData.email}
-                onChange={handleChange}
-                className="w-full p-3 bg-gray-800 text-gray-200 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-            </div>
-            <div>
-              <label className="block text-gray-200">Phone Number</label>
+          <div>
+            <div className="mb-4">
+              <label className="block text-gray-200">
+                <span className="text-gray-200">Name</span>
+              </label>
               <input
                 type="text"
-                name="phone"
+                name="name"
                 required
-                value={formData.phone}
+                value={formData.name}
                 onChange={handleChange}
                 className="w-full p-3 bg-gray-800 text-gray-200 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div>
+                <label className="block text-gray-200">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  required
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full p-3 bg-gray-800 text-gray-200 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-200">Phone Number</label>
+                <input
+                  type="text"
+                  name="phone"
+                  required
+                  value={formData.phone}
+                  onChange={handleChange}
+                  className="w-full p-3 bg-gray-800 text-gray-200 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label className="block text-gray-200">Message</label>
+              <textarea
+                name="message"
+                required
+                value={formData.message}
+                onChange={handleChange}
+                className="w-full p-3 h-32 bg-gray-800 text-gray-200 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full mt-4 py-3 bg-blue-500 text-gray-200 rounded-lg text-lg hover:bg-blue-600 transition-all"
+            >
+              Send Message
+            </button>
           </div>
-          <div className="mt-4">
-            <label className="block text-gray-200">Message</label>
-            <textarea
-              name="message"
-              required
-              value={formData.message}
-              onChange={handleChange}
-              className="w-full p-3 h-32 bg-gray-800 text-gray-200 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="w-full mt-4 py-3 bg-blue-500 text-gray-200 rounded-lg text-lg hover:bg-blue-600 transition-all"
-          >
-            Send Message
-          </button>
         </form>
       </div>
     </div>
